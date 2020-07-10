e58cb236f643bc07de6d58401a2bee8cffd22d2b
tober65 <timothy.ober@gmail.com>
2020-07-10 14:02:13 -0700

    paths to index.html and notes.html

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -7,6 +8,15 @@ const PORT = process.env.PORT || 3001;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  });
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
 
 // Starts the server to begin listening
 // =============================================================
