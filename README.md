37db634c4e3987cc61e4737ee09cc93e7e513f61
tober65 <timothy.ober@gmail.com>
2020-07-12 18:52:50 -0700

    Updated readme and removed console.logs

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# Note-Taker
-This is homework 11, a note taker with client and server
+# Note Taker
+
+## Description 
+This is homework 11, a note taker with a client and server
+
+<img src="./public/assets/images/note-taker.PNG">
+
+## Table of Contents 
+[Installation](#installation)
+
+[Usage](#usage)
+
+[License](#license)
+
+[Contributing](#contributing)
+
+[Tests](#tests)
+
+[Questions](#questions)
+## Installation
+Download the source code, then in the folder with server.js run "npm install".
+
+## Usage
+In the folder with server.js run "npm start". Then in a browser navigate to http://localhost:3001/.
+
+## License
+This is licensed under the [MIT license](https://choosealicense.com/licenses/mit/).
+
+## Contributing
+Feel free to contribute
+
+## Tests
+No tests at the moment
+
+## Questions
+GitHub: https://github.com/tober65
+Email: timothy.ober@gmail.com
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ function readNotesFromFile() {
         if (error) {
             return console.log(error);
         }
-        console.log("Reading: ", data);
+        // console.log("Reading: ", data);
         notes = JSON.parse(data);
     });
 }
@@ -27,7 +27,7 @@ function writeNotesToFile() {
         if (error) {
             return console.log(error);
         }
-        console.log("Writing", JSON.stringify(notes));
+        // console.log("Writing", JSON.stringify(notes));
     });
 }
 
@@ -63,13 +63,13 @@ app.post("/api/notes", (req, res) => {
     let newNote = req.body;
     newNote.id = getNextId();
 
-    console.log("Pushing", newNote);
+    // console.log("Pushing", newNote);
 
     notes.push(newNote);
 
     writeNotesToFile();
 
-    res.sendStatus(200);
+    return res.json(notes);
 
 });
 
@@ -78,7 +78,7 @@ app.delete("/api/notes/:id", (req, res) => {
     // This works because of our body parsing middleware
     let id = parseInt(req.params.id);
 
-    console.log("Deleting note " + id);
+    // console.log("Deleting note " + id);
 
     notes = notes.filter((elem) => {return elem.id !== id});
 
